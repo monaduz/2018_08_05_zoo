8cd9a52354745d9c17758f1911396227a5da45e8
monaduz <duzmonika@gmail.com>
2018-08-05 09:17:17 +0200

    Initial commit

---
+++ src/BlackBear.java
@@ -0,0 +1,5 @@
+public class BlackBear extends Bear {
+    public BlackBear(double weight, boolean isAlive) {
+        super( weight, isAlive );
+    }
+}
---
+++ src/ZooAplication.java
@@ -0,0 +1,5 @@
+public class ZooAplication {
+    public static void main(String[] args) {
+
+    }
+}
